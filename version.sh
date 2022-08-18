6a658f880474db8b0ce5cc2a5abdbc90fa3d72ea
Rajat Parashar <parasharrajat@users.noreply.github.com>
2022-08-18 06:20:27 +0530

    fix  script

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-version=$(grep version package.json | awk -F \" '{print $4}');
+version=$(grep -m1 version package.json | awk -F \" '{print $4}');
 
 sed -i "s/\"version\": \"[0-9\.]*/\"version\": \"$version/g" app/manifest.json
 
